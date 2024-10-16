572489a8080261df978d575b11ce0f7d92c94269
TejSutar8 <tejsutar8@gmail.com>
2024-10-16 19:10:02 +0530

    Update

--- Spils Code/Slip_12/singly.c
+++ Spils Code/Slip_12/singly.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 #include "singly.h"
-
 int main()
 {
-    
+    struct Node *head = NULL;
+    insert(&head, 10);
+    insert(&head, 20);
+    insert(&head, 30);
+    display(head);
+
+    delete (&head, 20);
+    display(head);
+
     return 0;
 }
--- Spils Code/Slip_12/singly.h
+++ Spils Code/Slip_12/singly.h
@@ -14,3 +14,52 @@ struct Node *create(int data)
     newNode->next = NULL;
     return newNode;
 }
+
+void insert(struct Node **head, int new_data)
+{
+    struct Node *new_node = create(new_data);
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return;
+    }
+    struct Node *last = *head;
+    while (last->next != NULL)
+        last = last->next;
+    last->next = new_node;
+}
+
+void display(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
+
+void delete(struct Node **head, int key)
+{
+    struct Node *temp = *head;
+    struct Node *prev = NULL;
+
+    if (temp != NULL && temp->data == key)
+    {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp != NULL && temp->data != key)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return;
+
+    prev->next = temp->next;
+    free(temp);
+}
